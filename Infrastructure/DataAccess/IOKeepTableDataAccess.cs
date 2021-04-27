a1f3bab3306cf98460b822c7d52313849c15f374
jimmy.nordin.1979@gmail.com <jimmy.nordin.1979@gmail.com>
2021-04-27 23:57:17 +0200

    210427 2357

--- Infrastructure/DataAccess/IOKeepTableDataAccess.cs
+++ Infrastructure/DataAccess/IOKeepTableDataAccess.cs
@@ -34,18 +34,71 @@ namespace Infrastructure.DataAccess
             return XCoordinateList;
         }
 
+
+        //DETTA ÄR EN DÅLIG LÖSNING. DEN ÄR TEMPORÄR. NÄR OM SYSTEMET GÅR LIVE KAN DET VARA 1000 SIGNALER...
+        //ANTINGEN GÖR JAG REFLEKTION
+        //ELLER SÅ NÅGON SQL/EF LÖSNING. KANSKE FÅR VI LÖSNING PÅ UTB...SÅ MYCKET ATT GÖRA OCH SÅ LITE TID...
         public List<Boolean> IOYCoordinatesFromSignal_FromIOKeepTable(string Signal)
         {
             List<Boolean> YCoordinateList = new List<Boolean>();
             List<IOSample> IOSampleList = eFAccessIOKeepTable.IOKeepTable.ToList();
 
-            //jimmyhaglund har lösning. Fixa asap...tight deadline...
-
-            //Hårdkodad tmp lösning.
-            foreach (IOSample item2 in IOSampleList)
+            foreach (IOSample iOSample in IOSampleList)
             {
-                YCoordinateList.Add(item2.Hub2Hub_KKS123_SystemVolt_Erratic);
-                //Debug.WriteLine($"item.Hub2Hub_KKS123_SystemVolt_Erratic = {item2.Hub2Hub_KKS123_SystemVolt_Erratic}");
+                switch (Signal)
+                {
+                    case "Hub2Hub_KKS123_AuxPressure_Low":
+                        YCoordinateList.Add(iOSample.Hub2Hub_KKS123_AuxPressure_Low);
+                        break;
+                    case "Hub2Hub_KKS123_Retarder_LowCurrent":
+                        YCoordinateList.Add(iOSample.Hub2Hub_KKS123_Retarder_LowCurrent);
+                        break;
+                    case "Hub2Hub_KKS123_SystemVolt_Erratic":
+                        YCoordinateList.Add(iOSample.Hub2Hub_KKS123_SystemVolt_Erratic);
+                        break;
+                    case "Hub2Hub_KKS123_SystemVolt_Low":
+                        YCoordinateList.Add(iOSample.Hub2Hub_KKS123_SystemVolt_Low);
+                        break;
+                    case "Panna_flisinmatning_skruv1_Motorskydd":
+                        YCoordinateList.Add(iOSample.Panna_flisinmatning_skruv1_Motorskydd);
+                        break;
+                    case "Panna_flisinmatning_skruv1_Nodstop":
+                        YCoordinateList.Add(iOSample.Panna_flisinmatning_skruv1_Nodstop);
+                        break;
+                    case "Panna_flisinmatning_skruv1_Sakerhetsbrytare":
+                        YCoordinateList.Add(iOSample.Panna_flisinmatning_skruv1_Sakerhetsbrytare);
+                        break;
+                    case "Panna_flisinmatning_skruv1_Varvtalsvakt":
+                        YCoordinateList.Add(iOSample.Panna_flisinmatning_skruv1_Varvtalsvakt);
+                        break;
+                    case "Karlatornet_Brandlarm_Hiss1_Aktivt":
+                        YCoordinateList.Add(iOSample.Karlatornet_Brandlarm_Hiss1_Aktivt);
+                        break;
+                    case "Karlatornet_Brandlarm_Hiss2_Aktivt":
+                        YCoordinateList.Add(iOSample.Karlatornet_Brandlarm_Hiss2_Aktivt);
+                        break;
+                    case "Karlatornet_Ventilation_Franluft_HogTemp":
+                        YCoordinateList.Add(iOSample.Karlatornet_Ventilation_Franluft_HogTemp);
+                        break;
+                    case "Karlatornet_Ventilation_Franluft_LagTemp":
+                        YCoordinateList.Add(iOSample.Karlatornet_Ventilation_Franluft_LagTemp);
+                        break;
+                    case "Vestas_Verk12_Koppling_HogTemp":
+                        YCoordinateList.Add(iOSample.Vestas_Verk12_Koppling_HogTemp);
+                        break;
+                    case "Vestas_Verk12_Koppling_LagOljeNiva":
+                        YCoordinateList.Add(iOSample.Vestas_Verk12_Koppling_LagOljeNiva);
+                        break;
+                    case "Vestas_Verk12_Koppling_TryckAvvikelse":
+                        YCoordinateList.Add(iOSample.Vestas_Verk12_Koppling_TryckAvvikelse);
+                        break;
+                    case "Vestas_Verk12_Vaderstation_WatchDog":
+                        YCoordinateList.Add(iOSample.Vestas_Verk12_Vaderstation_WatchDog);
+                        break;
+                    default:
+                        Debug.WriteLine($"In IOYCoordinatesFromSignal_FromIOKeepTable...");
+                        break;
+                }
             }
             return YCoordinateList;
         }
